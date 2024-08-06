52488fd61dd7cb41b6b63f518c00102c7f7d1867
ryosuke <tmcit.miyazaki@gmail.com>
2024-08-06 21:31:13 +0900

    fix

--- .github/scripts/format_changes.py
+++ .github/scripts/format_changes.py
@@ -2,21 +2,30 @@ import sys
 
 def format_changes(changes):
     formatted_lines = []
+    code_block_started = False
     for line in changes.split(r'\r\n'):
-        # Replace * with -
-        line = line.replace(r'*', '-')
-        
+        # Remove everything after "--Full Changelog--"
+        if line.startswith('**Full Changelog'):
+            break
+
         # Remove "by @username" and the URL until the end of the line
         if ' by @' in line:
             line = line.split(' by @')[0]
         
+        # Replace * with -
+        line = line.replace(r'*', '-')
+
+        # Replace ## with *text*
+        if line.startswith('##'):
+            line = '*' + line.replace('##', '').strip() + '*'
+
         formatted_lines.append(line)
+
+        if line == "*What's Changed*":
+            formatted_lines.append('```')
     
+    formatted_lines.append('```') 
     formatted_changes = r'\r\n'.join(formatted_lines)
-    
-    # Remove everything after "--Full Changelog--"
-    if '--Full Changelog--' in formatted_changes:
-        formatted_changes = formatted_changes.split('--Full Changelog--')[0]
     
     return formatted_changes.rstrip(r'\r\n')
 
