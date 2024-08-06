41ae45da8673f17820fb93b46f11cd06126b9d86
ryosuke <tmcit.miyazaki@gmail.com>
2024-08-06 21:02:40 +0900

    modify

---
+++ .github/scripts/format_changes.py
@@ -0,0 +1,30 @@
+import sys
+
+def format_changes(changes):
+    formatted_lines = []
+    for line in changes.split(r'\n'):
+        # Remove carriage returns
+        line = line.replace(r'\r', '')
+        
+        # Replace * with -
+        line = line.replace(r'*', '-')
+        
+        # Remove "by @username" and the URL until the end of the line
+        if ' by @' in line:
+            line = line.split(' by @')[0]
+        
+        formatted_lines.append(line)
+    
+    formatted_changes = '\n'.join(formatted_lines)
+    
+    # Remove everything after "--Full Changelog--"
+    if '--Full Changelog--' in formatted_changes:
+        formatted_changes = formatted_changes.split('--Full Changelog--')[0]
+    
+    return formatted_changes
+
+if __name__ == "__main__":
+    changes = sys.argv[1]
+    formatted_changes = format_changes(changes)
+    
+    print(formatted_changes)
